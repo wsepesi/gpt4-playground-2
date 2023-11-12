9ffe9510d106ae56c1642c3074fd98264d7de8d1
wsepesi <sepesi.w@wustl.edu>
2023-11-12 15:43:21 -0600

    test commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,7 +4,6 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
 
 const MODEL = 'gpt-4-1106-preview'
-// const MODEL = 'gpt-4-0613'
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
